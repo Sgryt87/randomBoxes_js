5fd92d474261f9d41d96a1c2b7d1ab2a4a0407dd
Sergii <sergiigrytsaienko@gmail.com>
2017-07-18 13:17:10 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  var items = document.querySelectorAll('.items .item ');
+
+  /*for(var i =0; i < items.length; i++){
+    items[i].onclick = activeItem;
+  }*/
+    function activeItem(){
+
+      this.classList.toggle('item-active');
+    }
+
+ setInterval(function(){
+     var rand = mtRand(0, items.length-1);
+
+    activeItem.call(items[rand]);
+   }, 500);
+  };
+
+function mtRand(min, max) {
+    return Math.floor(Math.random()*(max-min+1));
+  }
+
+console.log(mtRand);
